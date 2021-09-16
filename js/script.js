00abf68a3f829131256ef4eac5d3e916f863a20b
Gapragapr <gapragapr3@mail.ru>
2021-09-17 00:00:34 +0600

    add files

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ function popupOpen(open = true){
     const popupItem = document.getElementById('popup');
     popupItem.style.display = open ? 'flex' : 'none';
 }
+
+function hamburgerOpen(open = true){
+    const hamburger = document.getElementById('hamburger-menu');
+    hamburger.style.opacity = open ? '1' : '0';
+    hamburger.style.height = open ? '300px' : '0';
+}
